87831c41ad9e93254bd8d2410a21538bb5d77711
AVaamondeH <alexxherrera2772@gmail.com>
2023-09-25 02:44:02 -0500

    endpoints

--- server/src/server.js
+++ server/src/server.js
@@ -8,5 +8,16 @@ server.use(morgan("dev"));
 server.use(express.json());
 server.use(cors());
 server.use(router);
+server.use((req, res, next) => {
+    const allowedOrigins = ['http://localhost:3000', 'https://selpro-soluciones.netlify.app'];
+    const origin = req.headers.origin;
+  
+    if (allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    } res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
 
 module.exports = server;
